a95606d3f3d0a22e5cb0c89b588f6d1f1136f99d
Davyson de Azevedo <158609502+Azevd0@users.noreply.github.com>
2025-09-05 01:56:35 -0300

    Update LivroDAO.java

--- bancodedadosfucturajpaalunos-dev/bancodedadosfuctura/src/main/java/dao/LivroDAO.java
+++ bancodedadosfucturajpaalunos-dev/bancodedadosfuctura/src/main/java/dao/LivroDAO.java
@@ -37,27 +37,39 @@ public class LivroDAO {
 	    
 	    return minhalist;
 	}
-	public List<Livro> findByAutor(String autor) {
+	public List<Livro> findByField(String valor, String finder) {
 		EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
-		String jpql = "SELECT l FROM Livro l WHERE l.autor = :autor";
+		String jpql = "SELECT l FROM Livro l WHERE l."+finder+" = :valor";
 		TypedQuery<Livro> query = em.createQuery(jpql, Livro.class);
-		query.setParameter("autor", autor);
+		query.setParameter("valor", valor);
 		List<Livro> minhalist = query.getResultList();
 		em.close();
 		return minhalist;
 	}
+	public List<Livro> findByAutor(String autor) {
+	    EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+	    String jpql = "SELECT l FROM Livro l WHERE l.autor = :autor";
+	    TypedQuery<Livro> query = em.createQuery(jpql, Livro.class);
+	    query.setParameter("autor", autor);
+	    List<Livro> minhalist = query.getResultList();
+	    em.close();
+	    return minhalist;
+	}
 	
-	public void deleteLivro(Integer id) {
+	public boolean deleteLivro(Integer id) {
 		EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
 		Livro livro = em.find(Livro.class, id);
 		if (livro != null) {
 		    em.remove(livro);
+		    System.out.println("Livro removido com sucesso");
+		} else {
+			System.out.println("Não encontrado para remoção");
 		}
 		em.getTransaction().commit();
 		em.close();
+		return true;
 	}
-
 	public Livro updateLivro(Livro livro) {
 		EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
@@ -75,3 +87,4 @@ public class LivroDAO {
 	
 	
 	
+
